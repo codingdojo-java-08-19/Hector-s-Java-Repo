b6c2014dc66007bc6ba57294e899928dc1b05b6d
TheLegendBreaker <thegreatcarradio@yahoo.com>
2019-08-05 13:52:52 -0600

    swift and sweet

---
+++ 01-java-fundamentals/02-fundamentals/01-first-java-program/javaFun/HellowWorld.java
@@ -0,0 +1,7 @@
+public class HellowWorld {
+    public static void main(String[] args) {
+        System.out.println("My name is coding dojo");
+        System.out.println("I am 100 years old");
+        System.out.println("I live in Burbank Cali");
+    }
+}
