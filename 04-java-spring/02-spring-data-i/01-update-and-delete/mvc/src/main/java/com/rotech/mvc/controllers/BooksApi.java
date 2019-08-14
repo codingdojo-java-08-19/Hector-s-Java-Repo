8202832f77acb166f16a1e4613a8b1c5f6f843b6
TheLegendBreaker <thegreatcarradio@yahoo.com>
2019-08-14 16:39:08 -0600

    set up was smooth. everything takes forever todo the first time. following the instructions in the lectures and on the track I did not run into any problems in code. I did get hung up on mySQL server. I vaguely remember setting it up. but was not sure and there fore did not know if the MAMP was relevant to me.

---
+++ 04-java-spring/02-spring-data-i/01-update-and-delete/mvc/src/main/java/com/rotech/mvc/controllers/BooksApi.java
@@ -0,0 +1,48 @@
+package com.rotech.mvc.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rotech.mvc.models.Book;
+import com.rotech.mvc.services.BookService;
+
+@RestController
+@RequestMapping("/api/books")
+public class BooksApi {
+	private final BookService bookService;
+	
+	public BooksApi(BookService bookService) {
+		this.bookService = bookService;
+	}
+	@RequestMapping("")
+	public List<Book> index() {
+		return bookService.allBooks();
+	}
+	    
+	@RequestMapping(value="", method=RequestMethod.POST)
+	public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+		Book book = new Book(title, desc, lang, numOfPages);
+		return bookService.createBook(book);
+	}
+	    
+	@RequestMapping("/{id}")
+	public Book show(@PathVariable("id") Long id) {
+		Book book = bookService.findBook(id);
+		return book;
+	}
+	@RequestMapping(value="/{id}", method= RequestMethod.POST)
+	public Book update( @PathVariable("id") Long id,@RequestParam("title") String title, @RequestParam("description") String desc, @RequestParam("language") String lang, @RequestParam("pages") Integer pages) {
+		
+		Book book =  new Book(id,title, desc,lang,pages);
+				return bookService.update(book);
+	}
+	@RequestMapping("delete/{id}")
+	public void delete(@PathVariable("id") Long id) {
+		bookService.deleteById(id);
+	}
+}
